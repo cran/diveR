1196efe502233ff5fc7eb5ca73e55b07cf1e8715
R. Wayne Oldford <rwoldford@uwaterloo.ca>
2021-09-28 18:40:02 +0000

    version 0.1.2

--- R/attach.R
+++ R/attach.R
@@ -1,8 +1,10 @@
 ## Copied from file of the same name in tidyverse and modified for diveR
 
 diveRcore <- function() {
-  c("loon", "loon.data", "ggmulti", "loon.ggplot", "zenplots")
-  } #, "loon.tourr")
+  c("loon", "loon.data", "ggmulti",
+    "loon.ggplot", "zenplots", "loon.shiny",
+    "loon.tourr")
+  }
 
 presentationPackages  <- function() {
   c("cli", "crayon", "rstudioapi")
@@ -17,10 +19,15 @@ core_unloaded <- function() {
 # Attach the package from the same package library it was
 # loaded from before.
 same_library <- function(pkg) {
-  loc <- if (pkg %in% loadedNamespaces()) dirname(getNamespaceInfo(pkg, "path"))
+  loc <- if (pkg %in% loadedNamespaces()) {
+    dirname(getNamespaceInfo(pkg, "path"))
+    }
   do.call(
     "library",
-    list(pkg, lib.loc = loc, character.only = TRUE, warn.conflicts = FALSE)
+    list(pkg,
+         lib.loc = loc,
+         character.only = TRUE,
+         warn.conflicts = FALSE)
   )
 }
 
